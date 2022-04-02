2cc6cfe38481848612051f0c825bd322b7830e8b
Osh <gusmunos@student.gu.se>
2022-04-02 22:43:45 +0200

    adding dist

---
+++ dist/precache-manifest.53d5f40e9bccb450e81294d643f9e016.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "313e22c28a40c916d58a",
+    "url": "/css/app.9e865c0d.css"
+  },
+  {
+    "revision": "5157dd1aaa2321e08b33",
+    "url": "/css/chunk-vendors.f0372c03.css"
+  },
+  {
+    "revision": "d6f21dc1fbd18def3de423a101a252c1",
+    "url": "/img/coldDrink.d6f21dc1.jpg"
+  },
+  {
+    "revision": "59902df21a3f776f0da95f1f84990cb8",
+    "url": "/img/hotDrink.59902df2.jpg"
+  },
+  {
+    "revision": "8fff1f248103119466900ef67a527499",
+    "url": "/img/pic0.8fff1f24.jpg"
+  },
+  {
+    "revision": "6e3ea54390b83dfed36adeb4f49e43bf",
+    "url": "/img/pic1.6e3ea543.jpg"
+  },
+  {
+    "revision": "9a307aeb74b6723ccdf215c3b756b392",
+    "url": "/img/pic2.9a307aeb.jpg"
+  },
+  {
+    "revision": "a8aa749d1b7ef24faab72fec5909a910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "313e22c28a40c916d58a",
+    "url": "/js/app.cc290f75.js"
+  },
+  {
+    "revision": "5157dd1aaa2321e08b33",
+    "url": "/js/chunk-vendors.70bccee0.js"
+  },
+  {
+    "revision": "787809bb0a3e5690deb6de621ab40daf",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
